3817bd2ddeee76c079c8ce116d3947ec3a9c59cd
elamre <e.vanrijnswou@innoseis.com>
2017-03-01 20:19:25 +0100

    Manchester for packed decoding

---
+++ rtlsdr/src/Packet.h
@@ -0,0 +1,23 @@
+/*
+ * Packet.h
+ *
+ *  Created on: Mar 1, 2017
+ *      Author: elmar
+ */
+
+#ifndef PACKET_H_
+#define PACKET_H_
+#include <vector>
+
+class Packet {
+public:
+	Packet(uint16_t * binString, size_t len);
+	size_t getByteSize();
+	uint8_t getData(uint8_t buffer[]);
+	virtual ~Packet();
+private:
+	void manchesterDecode(uint16_t * binString, size_t len);
+	std::vector<uint8_t> byteData;
+};
+
+#endif /* PACKET_H_ */
