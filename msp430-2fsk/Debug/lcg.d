231a27b11cc14a2f547a1270d0c55c9040c06cfd
ClovsBob <clovis.bobillier@hotmail.fr>
2017-03-16 17:56:51 +0100

    msp code

---
+++ msp430-2fsk/Debug/lcg.d
@@ -0,0 +1,7 @@
+# FIXED
+
+lcg.obj: ../lcg.c
+lcg.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdint.h
+
+../lcg.c: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.0.LTS/include/stdint.h: 
