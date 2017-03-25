ed270ebb715d62ba54d7c6eb7af14b36a015cc52
s3xm3x <s3xm3x@gmx.com>
2017-03-25 13:36:42 +0100

    Deleted SW for MSP430.

--- msp430-2fsk/Debug/lcg.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-lcg.obj: ../lcg.c
-lcg.obj: /home/maximilian/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdint.h
-
-../lcg.c: 
-/home/maximilian/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdint.h: 
