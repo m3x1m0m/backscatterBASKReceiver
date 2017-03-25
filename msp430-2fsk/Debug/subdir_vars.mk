89ed589cdfede0f4ad5b55e4b3cc128d1270d0d1
s3xm3x <s3xm3x@gmx.com>
2017-03-25 13:40:29 +0100

    Adding the MSP430 SW in a proper way with .gitignore.

---
+++ msp430-2fsk/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5969.cmd 
+
+C_SRCS += \
+../lcg.c \
+../main.c 
+
+C_DEPS += \
+./lcg.d \
+./main.d 
+
+OBJS += \
+./lcg.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"lcg.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"lcg.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../lcg.c" \
+"../main.c" 
+
+
